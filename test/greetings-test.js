33688df26a342aa808f3565347c70afcb768caa8
sp010412 <smajwede7@gmail.com>
2021-09-07 09:54:31 +0200

    deploy using heroku postgres

--- test/greetings-test.js
+++ test/greetings-test.js
@@ -5,7 +5,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/travis_ci_test';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/test';
 
 const pool = new Pool({
     connectionString,
